eddea5a4caa832bdb84f015c2f0f7a358ce89154
nandezer <marchf25@gmail.com>
2021-03-22 23:06:09 +0100

    get product by handle not navigation menu link

--- AssetPBcheckbox-new.js
+++ AssetPBcheckbox-new.js
@@ -1,106 +1,109 @@
 {% assign planbooProduct = all_products['planboo-bamboo'] %}
 
-{% assign id = planbooProduct.variants.first.id %}
 
 
-{% assign bamboo_wraps_in_cart = 0 %}
+{% if planbooProduct.available == true%}
 
-{% for item in cart.items %}
-  {% if item.id == id %}
-    {% assign bamboo_wraps_in_cart = item.quantity %}
-  {% endif %}
-{% endfor %}
+  {% assign id = planbooProduct.variants.first.id %}	 
+  {% assign bamboo_wraps_in_cart = 0 %}
 
-{% if planbooProduct.variants.first.price >= 0 %}
+  {% for item in cart.items %}
+    {% if item.id == id %}
+      {% assign bamboo_wraps_in_cart = item.quantity %}
+    {% endif %}
+  {% endfor %}
+
+  <div id="is-a-bamboo" style="clear: left; margin: 0 0 0 0" class="clearfix rte">
+    <input id="planboo-bamboo" type="checkbox" name="attributes[planboo-bamboo]" {% if cart.attributes.planboo-bamboo %} value="yes" checked="checked"{% endif %} style="float: none" />
+    <label for="planboo-bamboo" style="display:inline; padding-left: 5px; float: none;">
+      Climate friendly delivery
+    </label>
+    {% render 'why-planboo' %}
+  </div>
+
+  <script>
+
+  Shopify.Cart = Shopify.Cart || {};
+
+  Shopify.Cart.Bamboo = {};
+
+  Shopify.Cart.Bamboo.set = function() {
+      var headers = new Headers({ 'Content-Type': 'application/json' });
+
+      console.log("Add bamboo product");
+
+      let formData = { updates: { {{ id }}: 1 }, attributes: { 'planboo-bamboo': 'true'} };
+
+     fetch('/cart/update.js', {
+       method: 'POST',
+       headers: headers,
+       body: JSON.stringify(formData)
+     })
+     .then(response => {
+       console.log("succes added");
+
+       return response.json();
+     })
+     .catch((error) => {
+       console.error('Error:', error);
+     });
+  }
+
+  Shopify.Cart.Bamboo.remove = function() {
+    var headers = new Headers({ 'Content-Type': 'application/json' });
+
+    console.log("remove bamboo product");
 
 
-<div id="is-a-bamboo" style="clear: left; margin: 0 0 0 0" class="clearfix rte">
-  <p>
+   let formData = { updates: { {{ id }}: 0 }, attributes: { 'planboo-bamboo': ''} };
 
-	
-	<label for="planboo-bamboo" style="display:inline; padding-left: 5px; float: none;">
-	Climate friendly delivery
-	</label>
-    <input id="planboo-bamboo" type="checkbox" name="attributes[planboo-bamboo]" value="yes" {% if cart.attributes.planboo-bamboo %} checked="checked"{% endif %} style="float: none" />
-  </p>
-</div>
-{% render 'why-planboo' %}
+   fetch('/cart/update.js', {
+     method: 'POST',
+     headers: headers,
+     body: JSON.stringify(formData)
+   })
+   .then(response => {
+      console.log("succes removal");
 
+     return response.json();
+   })
+   .catch((error) => {
+     console.error('Error:', error);
+   });
+  }
 
-<style>
-#updates_{{ id }} { display: none; }
-</style>
-
-<script>
-
-Shopify.Cart = Shopify.Cart || {};
-
-Shopify.Cart.Bamboo = {};
-
-Shopify.Cart.Bamboo.set = function() {
-  var headers = new Headers({ 'Content-Type': 'application/json' });
-  
-  var request = {
-    method: 'POST',
-    headers: headers,
-    body: JSON.stringify({ updates: { {{ id }}: 1 }, attributes: { 'planboo-bamboo': true } })
-  };
-  fetch('/cart/update.js', request)
-  .then(function() {
-    //location.reload();
-  });
-}
-
-Shopify.Cart.Bamboo.remove = function() {
-  var headers = new Headers({ 'Content-Type': 'application/json' });
-  
-  var request = {
-      method: 'POST',
-      headers: headers,
-      body: JSON.stringify({ updates: { {{ id }}: 0 }, attributes: { 'planboo-bamboo': '' } })
-  };
-  fetch('/cart/update.js', request)
-  .then(function() {
-    //location.reload();
-  });
-}
-  
-// If we have a planboo-bamboo item in the cart but our planboo-bamboo cart attribute has not been set.
-{% if bamboo_wraps_in_cart > 0 and cart.attributes.planboo-bamboo == blank  %}
-    document.addEventListener("DOMContentLoaded", function(){
-      console.log("add bamboo check");
-      Shopify.Cart.Bamboo.set();
+  // If we have a planboo-bamboo item in the cart but our planboo-bamboo cart attribute has not been set.
+  {% if bamboo_wraps_in_cart > 0 and cart.attributes.planboo-bamboo == blank  %}
+  document.addEventListener("DOMContentLoaded", function(){
+    console.log("remove bamboo check 1");
+    Shopify.Cart.Bamboo.remove();
   });
   // If we have no planboo-bamboo item in the cart but our planboo-bamboo cart attribute has been set.
-{% elsif bamboo_wraps_in_cart == 0 and cart.attributes.planboo-bamboo != blank  %}
+  {% elsif bamboo_wraps_in_cart == 0 and cart.attributes.planboo-bamboo != blank  %}
   document.addEventListener("DOMContentLoaded", function(){
-    console.log("remove bamboo check");
+  	console.log("remove bamboo check 2");
     Shopify.Cart.Bamboo.remove();
   });
 
-{% endif %}
+  {% endif %}
 
-
-
-
-// When the planboo-bamboo checkbox is checked or unchecked.
-document.addEventListener("click", function(){
-  document.querySelector('[name="attributes[planboo-bamboo]"]').addEventListener("change", function(event) {
-    if (event.target.checked) {
-      Shopify.Cart.Bamboo.set();
-    } else {
-      Shopify.Cart.Bamboo.remove();
-    }
+  document.addEventListener("click", function(){  
+  // When the planboo-bamboo checkbox is checked or unchecked.
+  	console.log("click");
+    document.querySelector('[name="attributes[planboo-bamboo]"]').addEventListener("change", function(event) {
+      if (event.target.checked) {
+          Shopify.Cart.Bamboo.set();
+      } else {
+          Shopify.Cart.Bamboo.remove();
+      }
+    });  
 
   });
-  
-});
-  
 
-</script>
+  </script>
 
-
-{% elsif planbooProduct.variants.first.price < 0 %}
+{% else %}
 
 
 {% endif %}
+
